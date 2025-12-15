3f908fcfe2bcf1ec849b3f367bca83439a6d2623
Sonofgrace2203 <114074856+Sonofgrace2203@users.noreply.github.com>
2025-12-15 02:59:41 -0800

    Add files via upload

--- cbt.js
+++ cbt.js
@@ -1,136 +1,27 @@
-const notifier = document.getElementById('notifier');
-const cancel = document.getElementById('cancel');
-const notified = document.getElementById("notified");
-
-function showNotification() {
-    notifier.style.display = 'd-block';
-};
-notified.addEventlistener('click', showNotification);
-
-function hideNotification() {
-    notifier.style.display = 'd-hide';
-};
-cancel.addEventlistener('click', hideNotification);
+// notification popup code
+const notified = document.querySelectorAll('.notified');
+const notifiedHide = document.querySelector('.notified-hide');
+const cancel = document.querySelectorAll('.cancel');
 
 
+notified.forEach(notified => notified.addEventListener('click', () =>
+    notifiedHide.style.display = 'block'
+));
+
+cancel.forEach(cancel => cancel.addEventListener('click', () =>
+    notifiedHide.style.display = 'none'
+));
 
 
+// mobile menu control code
+const mobileButton = document.querySelector('.mobile-menu-button');
+const mobileCancel = document.querySelectorAll('.cancel-mobile-menu');
+const mobileMenu = document.querySelector('.mobile');
 
+mobileButton.addEventListener('click', () =>
+    mobileMenu.style.display = 'block'
+);
 
-
-
-const mobileNav = document.querySelector('.header-menu');
-const mobileMenu = document.querySelector('.section-mobile-menu');
-const mobileCancel = document.querySelector('.mobile-cancel');
-const mobileCancel1 = document.querySelector('.main-body');
-
-function showMobileMenu() {
-    mobileMenu.style.display = 'block';
-}
-mobileNav.addEventListener('click', showMobileMenu);
-
-function hideMobileMenu() {
-    mobileMenu.style.display = 'none';
-}
-mobileCancel.addEventListener('click', hideMobileMenu);
-mobileCancel1.addEventListener('click', hideMobileMenu);
-
-
-const SLNredirect = document.getElementById('SLN-redirect-div')
-const SLNredirectFetch = document.getElementById('SLN-summaryBtn')
-const SLNredirectCancel = document.getElementById('SLN-redirect-cancel')
-
-function showSlnRedirect() {
-    SLNredirect.style.display = 'block';
-}
-SLNredirectFetch.addEventListener('click', showSlnRedirect);
-
-function hideSlnRedirect() {
-    SLNredirect.style.display = 'none';
-}
-SLNredirectCancel.addEventListener('click', hideSlnRedirect);
-
-
-
-const redirect = document.getElementById('redirect-div')
-const redirectFetch = document.getElementById('summaryBtn')
-const redirectCancel = document.getElementById('redirect-cancel')
-
-function showRedirect() {
-    redirect.style.display = 'block';
-}
-redirectFetch.addEventListener('click', showRedirect);
-
-function hideRedirect() {
-    redirect.style.display = 'none';
-}
-redirectCancel.addEventListener('click', hideRedirect);
-
-
-
-
-const exredirect = document.getElementById('ex-redirect-div')
-const exredirectFetch = document.getElementById('explainBtn')
-const exredirectCancel = document.getElementById('ex-redirect-cancel')
-
-function exShowRedirect() {
-    exredirect.style.display = 'block';
-}
-
-exredirectFetch.addEventListener('click', exShowRedirect);
-
-function exHideRedirect() {
-    exredirect.style.display = 'none';
-}
-exredirectCancel.addEventListener('click', exHideRedirect);
-
-
-
-
-const quredirect = document.getElementById('qu-redirect-div')
-const quredirectFetch = document.getElementById('quizBtn')
-const quredirectCancel = document.getElementById('qu-redirect-cancel')
-
-function quShowRedirect() {
-    quredirect.style.display = 'block';
-}
-
-quredirectFetch.addEventListener('click', quShowRedirect);
-
-function quHideRedirect() {
-    quredirect.style.display = 'none';
-}
-quredirectCancel.addEventListener('click', quHideRedirect);
-
-
-
-const pdredirect = document.getElementById('pd-redirect-div')
-const pdredirectFetch = document.getElementById('pdfBtn')
-const pdredirectCancel = document.getElementById('pd-redirect-cancel')
-
-function pdShowRedirect() {
-    pdredirect.style.display = 'block';
-}
-
-pdredirectFetch.addEventListener('click', pdShowRedirect);
-
-function pdHideRedirect() {
-    pdredirect.style.display = 'none';
-}
-pdredirectCancel.addEventListener('click', pdHideRedirect);
-
-
-
-const iframe = document.getElementById('iframe-con')
-const iframeBtn = document.getElementById('iframeBtn')
-const iframeCancel = document.getElementById('iframe-cancel')
-
-function showIframe() {
-    iframe.style.display = 'block';
-}
-iframeBtn.addEventListener('click', showIframe);
-
-function hideIframe() {
-    iframe.style.display = 'none';
-}
-iframeCancel.addEventListener('click', hideIframe);
+mobileCancel.forEach(cancel => cancel.addEventListener('click', () =>
+    mobileMenu.style.display = 'none'
+));
